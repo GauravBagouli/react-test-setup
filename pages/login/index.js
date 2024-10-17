ce7115a7d044c070d78ff2e681d4611720bdb327
Gaurav <gaurav.bagouli@techsuperiors.com>
2024-10-17 14:20:16 +0530

    login page added

---
+++ pages/login/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import dynamic from 'next/dynamic';
+const LoginComponent = dynamic(
+  () => import('@/components/common/login/index'),
+  { ssr: false },
+);
+
+export default function Signup() {
+  return <LoginComponent />;
+}
