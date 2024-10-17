e0adec726961199ae3e95a599d26f7dd70116d8c
Gaurav <gaurav.bagouli@techsuperiors.com>
2024-10-17 11:12:22 +0530

    initial commit

---
+++ next.config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  output: 'export',
+  reactStrictMode: false,
+  images: { unoptimized: true },
+  env: {
+    BASE_URL: process.env.HOST,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  transpilePackages: [
+    'lodash-es',
+    'lodash',
+    'geist',
+    '@ant-design',
+    '@rc-component',
+    'antd',
+    'rc-cascader',
+    'rc-checkbox',
+    'rc-collapse',
+    'rc-dialog',
+    'rc-drawer',
+    'rc-dropdown',
+    'rc-field-form',
+    'rc-image',
+    'rc-input',
+    'rc-input-number',
+    'rc-mentions',
+    'rc-menu',
+    'rc-motion',
+    'rc-notification',
+    'rc-pagination',
+    'rc-picker',
+    'rc-progress',
+    'rc-rate',
+    'rc-resize-observer',
+    'rc-segmented',
+    'rc-select',
+    'rc-slider',
+    'rc-steps',
+    'rc-switch',
+    'rc-table',
+    'rc-tabs',
+    'rc-textarea',
+    'rc-tooltip',
+    'rc-tree',
+    'rc-tree-select',
+    'rc-upload',
+    'rc-util',
+  ],
+  experimental: {
+    esmExternals: false,
+    scrollRestoration: true,
+    turbo: {
+      resolveAlias: {
+        underscore: 'lodash',
+        mocha: { browser: 'mocha/browser-entry.js' },
+      },
+      resolveExtensions: [
+        '.mdx',
+        '.tsx',
+        '.ts',
+        '.jsx',
+        '.js',
+        '.mjs',
+        '.json',
+      ],
+    },
+  },
+  compiler: {
+    styledComponents: true,
+  },
+
+  webpack: (config) => {
+    config.cache = false;
+    config.resolve.alias.canvas = false;
+    config.resolve.extensionAlias = {
+      '.js': ['.js', '.ts', '.tsx'],
+    };
+    return config;
+  },
+};
