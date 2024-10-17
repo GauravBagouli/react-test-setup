78c8d5d70703eff1126b517945a59b5170881099
diwakar singh <diwakar.singh@techsuperiors.com>
2024-10-17 15:58:46 +0530

    address page added in signup

---
+++ pages/signup/address/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import dynamic from 'next/dynamic';
+const AddressComponent = dynamic(() => import('@/components/common/signup/address'),{ ssr: false });
+
+export default function Signup() {
+  return (
+     <AddressComponent/>
+  )
+}
